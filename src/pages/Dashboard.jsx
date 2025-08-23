89e0932130a994ff60f5e51ae09aa9990d831f18
Jhon Rommel Golandrina <romsyaintmad@gmail.com>
2025-08-23 19:17:45 +0800

    changed designs

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 const Dashboard = () => {
+  const Data = [
+    { Quantity: "40", Label: "Applied" },
+    { Quantity: "15", Label: "Interviewing" },
+    { Quantity: "03", Label: "Offers" },
+    { Quantity: "37", Label: "Rejected" },
+  ];
+
   return (
-    <div>
-      <div className="text-4xl text-black">hello world</div>
+    <div className="w-full h-full flex flex-col gap-4 overflow-y-scroll">
+      <div className="w-full h-44 bg-[#222222] rounded-xl"></div>
+      <div className="w-full h-[420px] flex flex-row gap-4">
+        <div className="w-1/2 h-full flex flex-col gap-4">
+          <div className="w-full h-full border-2 border-neutral-200 rounded-xl"></div>
+          <div className="w-full h-full border-2 border-neutral-200 rounded-xl"></div>
+        </div>
+        <div className="w-1/2 h-full border-2 border-neutral-200 rounded-xl"></div>
+      </div>
+      <div className="w-full h-[600px] border-2 border-neutral-200 rounded-xl"></div>
     </div>
   );
 };
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,41 +1,87 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { MdOutlineDashboard, MdBallot, MdBorderColor } from "react-icons/md";
+import {
+  MdDashboard,
+  MdBallot,
+  MdBorderColor,
+  MdOutlineBrightness5,
+  MdOutlineBrightness2,
+} from "react-icons/md";
 
 const Layout = () => {
   const NavLinks = [
-    { label: "Dashboard", path: "/dashboard", icon: MdOutlineDashboard },
+    { label: "Dashboard", path: "/dashboard", icon: MdDashboard },
     { label: "Application", path: "/application", icon: MdBallot },
     { label: "Resources", path: "/resources", icon: MdBorderColor },
   ];
 
   return (
-    <div className="w-full min-h-screen bg-[#1B2122] flex flex-row transition-all duration-300">
-      {/* Side Bar */}
-      <aside className="w-[10%] min-h-screen bg-[#1B2122] py-6 flex flex-col items-center">
-        <div className="min-h-[6%] flex flex-row items-center justify-left mb-16 ">
-          <h1 className="text-left font-bold font-ubuntu text-4xl">trckr.</h1>
+    <div className="w-full h-screen transition-all duration-300 bg-[#fefffe]">
+      <div className="h-full flex flex-row">
+        {/* Sidebar */}
+        <div className="w-[290px] h-full flex flex-col py-6 px-4 justify-between overflow-hidden">
+          <div className="flex flex-col">
+            {/* Logo */}
+            <h1 className="text-5xl text-center text-[#222222] font-ubuntu font-bold">
+              trckr.
+            </h1>
+            {/* Divider */}
+            <div className="my-10 border-t-2 border-neutral-100" />
+            {/* Navigation Links */}
+            <ul className="text-sm text-[#222222] flex flex-col gap-4">
+              {NavLinks.map(({ label, path, icon: Icon }) => (
+                <a
+                  key={path}
+                  href={path}
+                  className="w-full gap-3 font-montserrat text-normal font-semibold px-4 py-2 text-[#222222] hover:text-white hover:bg-[#222222] rounded-lg cursor-pointer flex items-center"
+                >
+                  <Icon size={25} />
+                  <span className="">{label}</span>
+                </a>
+              ))}
+            </ul>
+          </div>
+          <div className="flex flex-col">
+            <div className="my-8 border-t-2 border-neutral-100" />
+            <div className="flex flex-row gap-4 items-center justify-center">
+              <div class="flex flex-row items-center gap-2">
+                <img
+                  src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp"
+                  className="rounded-full w-14"
+                />
+                <div className="flex flex-col text-sm text-[#222222] font-montserrat">
+                  <h1 className="text-[#222222]">John Doe</h1>
+                  <p className="text-neutral-400">Front-end Developer</p>
+                </div>
+              </div>
+              <label className="swap swap-rotate text-[#222222]">
+                {/* this hidden checkbox controls the state */}
+                <input type="checkbox" />
+
+                {/* sun icon */}
+                <MdOutlineBrightness5 size={20} className="swap-on" />
+
+                {/* moon icon */}
+                <MdOutlineBrightness2 size={20} className="swap-off" />
+              </label>
+            </div>
+          </div>
         </div>
-        <div className="w-52 min-h-[94%] flex flex-col items-center ">
-          <ul className="pl-7 w-full flex flex-col gap-4">
-            {NavLinks.map(({ label, path, icon: Icon }) => (
-              <a
-                key={path}
-                href={path}
-                className="w-full gap-2 font-montserrat text-normal font-normal px-4 py-2 text-gray-200 cursor-pointer hover:text-[#1B2122] hover:bg-white rounded-bl-lg rounded-tl-lg flex items-center"
-              >
-                <Icon size={15} />
-                <span className="">{label}</span>
-              </a>
-            ))}
-          </ul>
-        </div>
-      </aside>
-      {/* Children Pages */}
-      <div className="w-[90%] min-h-screen bg-[#1B2122] flex flex-col py-6 pr-6 gap-6">
-        <div className="w-full h-16 bg-white rounded-lg p-2 flex flex-row"></div>
-        <div className="w-full h-full bg-white rounded-lg">
-          <Outlet />
+        <div className="flex-1 h-full flex flex-col bg-[#fafbfb] py-6 px-8 gap-4 overflow-auto">
+          <div className=" font-montserrat">
+            <h1 className="text-4xl text-[#222222] font-bold">
+              Good morning, John👋
+            </h1>
+            <p className="text-lg text-neutral-400">
+              Welcome to your jobs dashboard
+            </p>
+          </div>
+          <div className="w-full h-full">
+            <Outlet />
+          </div>
+          <h1 className="text-center text-sm text-neutral-400 font-montserrat">
+            ©2025 Jhon Rommel Golandrina. All rights reserved.
+          </h1>
         </div>
       </div>
     </div>
